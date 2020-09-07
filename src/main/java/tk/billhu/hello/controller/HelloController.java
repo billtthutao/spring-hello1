86f65e1a7e6e892f1e1e8862e22d4d7f53cf467f
billhu <billhu@centos7.localdomain>
2020-09-07 13:36:14 +0800

    update controller

--- src/main/java/tk/billhu/hello/controller/HelloController.java
+++ src/main/java/tk/billhu/hello/controller/HelloController.java
@@ -1,13 +1,18 @@
 package tk.billhu.hello.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private Environment env;
+
     @RequestMapping("/")
     public String hello(){
-        String name = System.getProperty("MY_NAME");
+        String name = env.getProperty("path");
         return "Hello World From Spring-Hello1,MY_NAME="+name;
     }
 }
