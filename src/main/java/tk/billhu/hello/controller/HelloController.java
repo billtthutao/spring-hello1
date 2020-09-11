fd4b36719ed9b991e6fd598c112e8736d778707c
billhu <billhu@centos7.localdomain>
2020-09-11 09:18:05 +0800

    add health check

--- src/main/java/tk/billhu/hello/controller/HelloController.java
+++ src/main/java/tk/billhu/hello/controller/HelloController.java
@@ -2,17 +2,38 @@ package tk.billhu.hello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 public class HelloController {
     @Autowired
     private Environment env;
 
+    private static Random random = new Random();
+    private HttpStatus[] httpStatuses = new HttpStatus[]{HttpStatus.OK,HttpStatus.INTERNAL_SERVER_ERROR};
+    private HttpStatus httpStatus = httpStatuses[random.nextInt(2)];
+
     @RequestMapping("/")
     public String hello(){
         String name = env.getProperty("MY_NAME");
         return "Hello World From Spring-Hello1,MY_NAME="+name;
     }
+
+    @RequestMapping("/healthy")
+    public ResponseEntity healthy(){
+
+        return new ResponseEntity<>("Health Check...", httpStatus);
+    }
+
+    @RequestMapping("/ready")
+    public String ready(){
+
+        return hello();
+    }
 }
+
